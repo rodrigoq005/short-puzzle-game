d97ed8338f9e43c4fb1aec165070f3b367f518b4
rodrigoq005 <rodrigoq@udel.edu>
2026-04-08 14:43:04 -0400

    Character and interact parent class created
    Created 2d character following the interact pattern and created the interact parent class

---
+++ character_body_2d.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+var nearby_interactables = []
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+		
+	#Gets input directions
+	input_vector.x = Input.get_axis("ui_left", "ui_right")
+	input_vector.y = Input.get_axis("ui_up", "ui_down")
+	
+	#Ensures moving diagonal isn't faster than normal speed
+	input_vector = input_vector.normalized()
+	
+	if Input.is_action_just_pressed("interact"):
+		if nearby_interactables:
+			nearby_interactables.back().interact()
+	
+	velocity = input_vector * SPEED
+	
+	move_and_slide()
+
+
+func _on_interaction_detector_area_entered(area: Area2D) -> void:
+	print("interactable detected")
+	area.set_active(true)
+	nearby_interactables.append(area)
+
+
+func _on_interaction_detector_area_exited(area: Area2D) -> void:
+	print("interactable removed")
+	area.set_active(false)
+	nearby_interactables.erase(area)
---
+++ interactables.gd
@@ -0,0 +1,5 @@
+class_name Interactable extends Area2D
+
+
+func interact():
+		push_warning("No interact() code yet")
