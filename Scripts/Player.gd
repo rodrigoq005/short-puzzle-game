c08759584a843372e440c40a1f59ff709f2fd6f9
rodrigoq005 <rodrigoq@udel.edu>
2026-04-20 14:14:08 -0400

    Test

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -34,8 +34,8 @@ func _on_interaction_detector_area_exited(area: Area2D) -> void:
 	#area.set_active(false)
 	nearby_interactables.erase(area)
 
-if Input.is_action_just_pressed("interact"):
-	print("E PRESSED")
-	if nearby_interactables:
-		print("INTERACTING WITH:", nearby_interactables.back().name)
-		nearby_interactables.back().interact()
+#if Input.is_action_just_pressed("interact"):
+	#print("E PRESSED")
+	#if nearby_interactables:
+		#print("INTERACTING WITH:", nearby_interactables.back().name)
+		#nearby_interactables.back().interact()
